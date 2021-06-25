17c47ddba38ebd905db97284d3991e1261c74fcb
Bateristico <camilo.saa@gmail.com>
2021-06-25 22:14:39 +0200

    passing arguments

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 import requests
 
-BASE = "http://127.0.0.1:5000"
+BASE = "http://127.0.0.1:5000/"
 
-response = requests.get(BASE + "helloworld")
+response = requests.get(BASE + "helloworld/camilo/30")
 print(response.json())
+
